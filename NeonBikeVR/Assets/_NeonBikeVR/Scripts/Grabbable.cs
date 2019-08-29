9586e1e22750ea2f37b6a511eb7ed39b5904a86a
Jan Löhr <janloehr@live.com>
2019-08-29 23:28:18 +0200

    better abstraction of grabbing

---
+++ NeonBikeVR/Assets/_NeonBikeVR/Scripts/Grabbable.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Grabbable : OVRGrabbable
+{
+    [Header("Parameters")]
+    public bool LockPosition;
+    public Vector3 LockRotationAxes;
+
+    [Header("Events")]
+    public UnityEvent GrabStart;
+    public UnityEvent GrabStop;
+
+    public Transform Grabber { get; set; }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        m_grabbedBy = hand;
+        m_grabbedCollider = grabPoint;
+
+        Grabber = m_grabbedBy.transform;
+
+        if (m_grabbedBy is CustomGrabber grabber)
+        {
+            if (LockPosition)
+            {
+                grabber.LockPosition();
+
+                grabber.SetDontMoveGrabbable();
+            }
+
+            if (LockRotationAxes.magnitude > 1)
+            {
+                grabber.LockRotationAxes(LockRotationAxes.x > 0 ? true : false,
+                                            LockRotationAxes.y > 0 ? true : false,
+                                            LockRotationAxes.z > 0 ? true : false);
+            }
+
+            GrabStart.Invoke();
+        }
+    }
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        if (m_grabbedBy is CustomGrabber grabber)
+        {
+            grabber.ReleasePosition();
+            grabber.ReleaseRotationAxes();
+
+            GrabStop.Invoke();
+        }
+
+        Grabber = null;
+        
+        m_grabbedBy = null;
+        m_grabbedCollider = null;
+    }
+}
---
+++ NeonBikeVR/Assets/_NeonBikeVR/Scripts/MotorcycleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorcycleController : MonoBehaviour
+{
+    [Header("Object Refs")]
+    public Transform PlayerController;
+    public Transform ThrottleLever;
+
+    private Transform _trans;
+    
+    private bool _isDriving;
+    private Transform _grabber;
+    private Vector3 _initialThrottleDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trans = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isDriving && _initialThrottleDir.y < _grabber.forward.y)
+        {
+            _trans.position += _trans.forward * (_grabber.forward.y - _initialThrottleDir.y) * Time.deltaTime;
+        }
+    }
+
+    public void ThrottleGrabbed()
+    {
+        _grabber = ThrottleLever.GetComponent<Grabbable>().Grabber;
+        _initialThrottleDir = _grabber.forward;
+        PlayerController.parent = transform;
+
+        _isDriving = true;
+    }
+
+    public void ThrottleReleased()
+    {
+        _isDriving = false;
+        PlayerController.parent = null;
+    }
+}
