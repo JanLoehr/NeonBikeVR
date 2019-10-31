3ec16df610c6d008374a8c749fc62c44b49195ab
Jan Löhr <jan.loehr@sovanta.com>
2019-10-31 16:43:35 +0100

    progress

--- NeonBikeVR/Assets/_NeonBikeVR/Scripts/MotorcycleController.cs
+++ NeonBikeVR/Assets/_NeonBikeVR/Scripts/MotorcycleController.cs
@@ -5,38 +5,61 @@ using UnityEngine;
 public class MotorcycleController : MonoBehaviour
 {
     [Header("Params")]
-    public float SpeedMulti = 10;
-    public float RotMulti = 5;
+    public float Acceleration = 10;
+    public float MaxSteer = 20;
+    public float HeadWeightFactor = 10;
+
+    public float UpForceModifier = 10000;
 
     [Header("Object Refs")]
     public Transform PlayerController;
     public Transform ThrottleLever;
 
+    public Transform HeadWeight;
+    public Transform CenterOfMass;
+
+    public WheelCollider FrontWheel;
+    public WheelCollider BackWheel;
+
     private Transform _trans;
-    
+    private Rigidbody _rigidBody;
+
     private bool _isDriving;
-    
+
     private Transform _grabber;
     private Vector3 _initialThrottleDir;
 
     private Transform _headTransform;
+    private Vector3 _initialHeadPos;
 
     // Start is called before the first frame update
     void Start()
     {
         _trans = transform;
+        _rigidBody = GetComponent<Rigidbody>();
         _headTransform = Camera.main.transform;
+
+        _rigidBody.centerOfMass = CenterOfMass.localPosition;
+
+        _initialHeadPos = _headTransform.localPosition;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        float throttle = Input.GetAxis("Vertical");
+        float steer = Input.GetAxis("Horizontal");
+
         if (_isDriving && _initialThrottleDir.y < _grabber.forward.y)
         {
-            _trans.position += _trans.forward * (_grabber.forward.y - _initialThrottleDir.y) * Time.deltaTime * SpeedMulti;
-
-            _trans.Rotate(Vector3.up, _headTransform.localPosition.x * RotMulti);
+            throttle = (_grabber.forward.y - _initialThrottleDir.y) * Time.fixedDeltaTime * Acceleration;
         }
+
+        BackWheel.motorTorque = Acceleration * throttle * Time.fixedDeltaTime;
+        FrontWheel.steerAngle = MaxSteer * steer;
+        HeadWeight.localPosition = new Vector3((_headTransform.localPosition.x - _initialHeadPos.x) * HeadWeightFactor, HeadWeight.localPosition.y, HeadWeight.localPosition.z);
+
+        _rigidBody.AddTorque(_trans.forward * -_trans.localRotation.z * UpForceModifier * _rigidBody.velocity.magnitude);
     }
 
     public void ThrottleGrabbed(OVRGrabber hand)
